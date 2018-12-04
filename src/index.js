7e30eed1fde2f4aadeec8eb26a87466d8b8ef908
유정혁 (codigm) <junghyuk.yoo@goorm.io>
2018-12-04 08:35:04 +0000

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import path from "path";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
