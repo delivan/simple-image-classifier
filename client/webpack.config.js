de75bb2c2bcc304dd1bc429f62518e7293081826
hyuk22 <wjdgurdl272@gmail.com>
2018-12-04 23:55:31 +0900

    Refactoring

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,23 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src", "app.js"),
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"]
+          }
+        }
+      }
+    ]
   }
 };
